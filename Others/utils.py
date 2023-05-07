de31ef0df2986702fc663df76cf082d862ee4375
Samuel-Frachon <samuel.frachon@gmail.com>
2023-05-07 13:16:12 +0200

    Add of a class "Contract" ?

--- Others/utils.py
+++ Others/utils.py
@@ -6,3 +6,12 @@ Created on Sun Apr 30 20:44:09 2023
 
 def expectation(past, expected_past, l = 0.5):
     return expected_past + l*(past-expected_past)
+
+class Contract():
+    """ peut-être créer une classe pour les contrats ?"""
+    
+    def __init__(self,identity=int(0), amount=float(0)):
+        self.identity = identity
+        self.amount = amount
+
+    pass
